e594f77784e7d3f0e9ea0953daae68950bc36109
masiboss <32394683+masiboss@users.noreply.github.com>
2024-12-02 00:15:03 +0100

    feat: implement part1 and part2 functions with tests, update input handling in template

--- src/bin/NN.rs
+++ src/bin/NN.rs
@@ -1,46 +1,74 @@
+use adv_code_2024::*;
 use anyhow::*;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
 use code_timing_macros::time_snippet;
 use const_format::concatcp;
-use adv_code_2024::*;
+use itertools::Itertools;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
-const DAY: &str = "NN"; // TODO: Fill the day
+const DAY: &str = "01"; // TODO: Fill the day
 const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
 
-const TEST: &str = "\
-<TEST-INPUT>
-"; // TODO: Add the test input
+#[allow(dead_code)]
+struct Testdata {
+    input: &'static str,
+    test_result_1: usize,
+    test_result_2: usize,
+}
+
+#[allow(dead_code)]
+const TEST: Testdata = Testdata {
+    input: "\
+<INPUT>
+",
+    test_result_1: 0,
+    test_result_2: 0,
+};
 
 fn main() -> Result<()> {
     start_day(DAY);
 
     println!("=== Part 1 ===");
 
-    fn part1<R: BufRead>(reader: R) -> Result<usize> {
-        // TODO: Solve Part 1 of the puzzle
-        let answer = reader.lines().map_while(Result::ok).count();
-        Ok(answer)
-    }
-
-    // TODO: Set the expected answer for the test input
-    assert_eq!(0, part1(BufReader::new(TEST.as_bytes()))?);
-
     let input_file = BufReader::new(File::open(INPUT_FILE)?);
     let result = time_snippet!(part1(input_file)?);
     println!("Result = {}", result);
 
-    // println!("\n=== Part 2 ===");
-    //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    //     Ok(0)
-    // }
-    //
-    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
-    //
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = time_snippet!(part2(input_file)?);
-    // println!("Result = {}", result);\
+    println!("\n=== Part 2 ===");
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
 
     Ok(())
+}
+
+fn part1<R: BufRead>(reader: R) -> Result<usize> {
+    let answer = reader.lines().map_while(Result::ok).count();
+    Ok(answer)
+}
+
+fn part2<R: BufRead>(reader: R) -> Result<usize> {
+    let answer = reader.lines().map_while(Result::ok).count();
+    Ok(answer)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(
+            TEST.test_result_1,
+            time_snippet!(part1(BufReader::new(TEST.input.as_bytes())).unwrap())
+        );
+    }
+    #[test]
+    fn test_part2() {
+        assert_eq!(
+            TEST.test_result_2,
+            time_snippet!(part2(BufReader::new(TEST.input.as_bytes())).unwrap())
+        );
+    }
 }
