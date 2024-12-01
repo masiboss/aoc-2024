c901f617f01a027b8b252bfc2f6362e7575de2fa
masiboss <32394683+masiboss@users.noreply.github.com>
2024-12-01 23:12:14 +0100

    comp[01-1]: part one complete

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,15 +1,21 @@
+use adv_code_2024::*;
 use anyhow::*;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
 use code_timing_macros::time_snippet;
 use const_format::concatcp;
-use adv_code_2024::*;
+use itertools::Itertools;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
-const DAY: &str = "NN"; // TODO: Fill the day
+const DAY: &str = "01"; // TODO: Fill the day
 const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
 
 const TEST: &str = "\
-<TEST-INPUT>
+3   4
+4   3
+2   5
+1   3
+3   9
+3   3
 "; // TODO: Add the test input
 
 fn main() -> Result<()> {
@@ -17,14 +23,30 @@ fn main() -> Result<()> {
 
     println!("=== Part 1 ===");
 
-    fn part1<R: BufRead>(reader: R) -> Result<usize> {
+    fn part1<R: BufRead>(reader: R) -> Result<u32> {
         // TODO: Solve Part 1 of the puzzle
-        let answer = reader.lines().map_while(Result::ok).count();
+        let (mut left, mut right): (Vec<u32>, Vec<u32>) = reader
+            .lines()
+            .map_while(Result::ok)
+            .map(|line| -> (u32, u32) {
+                let mut nums = line.split_whitespace().map(|n| n.parse::<u32>().ok());
+                let a = nums.next().flatten();
+                let b = nums.next().flatten();
+                (a.unwrap(), b.unwrap())
+            })
+            .unzip();
+        left.sort();
+        right.sort();
+        let answer: u32 = left
+            .iter()
+            .zip(right.iter())
+            .map(|(a, b)| a.abs_diff(*b))
+            .sum();
         Ok(answer)
     }
 
     // TODO: Set the expected answer for the test input
-    assert_eq!(0, part1(BufReader::new(TEST.as_bytes()))?);
+    assert_eq!(11, part1(BufReader::new(TEST.as_bytes()))?);
 
     let input_file = BufReader::new(File::open(INPUT_FILE)?);
     let result = time_snippet!(part1(input_file)?);
