5ba3f940fd74201a6d97e7679303176be55af702
masiboss <32394683+masiboss@users.noreply.github.com>
2024-12-03 18:48:19 +0100

    comp[03-02]: complete part 2

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -3,7 +3,7 @@ use anyhow::*;
 use code_timing_macros::time_snippet;
 use const_format::concatcp;
 use itertools::Itertools;
-use regex::{Captures, RegexSet};
+use regex::{Captures, Regex};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
@@ -13,14 +13,18 @@ const TEST: Testdata = Testdata {
     input: "\
 xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
 ",
+    input2: "\
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+",
     test_result_1: 161,
-    test_result_2: 0,
+    test_result_2: 48,
 };
 const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
 
 #[allow(dead_code)]
 struct Testdata {
     input: &'static str,
+    input2: &'static str,
     test_result_1: usize,
     test_result_2: usize,
 }
@@ -46,8 +50,8 @@ fn main() -> Result<()> {
 fn part1<R: BufRead>(mut reader: R) -> Result<usize> {
     let mut buf = String::new();
     reader.read_to_string(&mut buf)?;
-    let regex = regex::Regex::new(r"mul\((\d+),\s*(\d+)\)")?;
-    let answer = regex
+    let mul = Regex::new(r"mul\((\d+),\s*(\d+)\)")?;
+    let answer = mul
         .captures_iter(&buf)
         .map(|capture: Captures| {
             let a = capture[1].parse::<usize>().unwrap();
@@ -58,8 +62,34 @@ fn part1<R: BufRead>(mut reader: R) -> Result<usize> {
     Ok(answer)
 }
 
-fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    let answer = reader.lines().map_while(Result::ok).count();
+fn part2<R: BufRead>(mut reader: R) -> Result<usize> {
+    let mut buf = String::new();
+    reader.read_to_string(&mut buf)?;
+    let mul = Regex::new(r"mul\((\d+),\s*(\d+)\)")?;
+    let do_regex = Regex::new(r"do\(\)")?;
+    let dont_regex = Regex::new(r"don't\(\)")?;
+    let answer = mul
+        .captures_iter(&buf)
+        .filter(|capture: &Captures| {
+            let preceding_text = &buf[..capture.get(0).unwrap().start()];
+            let last_do_idx = do_regex
+                .find_iter(preceding_text)
+                .last()
+                .map(|m| m.end())
+                .unwrap_or(0);
+            let last_dont_idx = dont_regex
+                .find_iter(preceding_text)
+                .last()
+                .map(|m| m.end())
+                .unwrap_or(0);
+            last_do_idx >= last_dont_idx
+        })
+        .map(|capture: Captures| {
+            let a = capture[1].parse::<usize>().unwrap();
+            let b = capture[2].parse::<usize>().unwrap();
+            a * b
+        })
+        .sum();
     Ok(answer)
 }
 
@@ -78,7 +108,7 @@ mod tests {
     fn test_part2() {
         assert_eq!(
             TEST.test_result_2,
-            time_snippet!(part2(BufReader::new(TEST.input.as_bytes())).unwrap())
+            time_snippet!(part2(BufReader::new(TEST.input2.as_bytes())).unwrap())
         );
     }
 }
