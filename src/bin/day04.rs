581e212b6ba62520771ec4ffcd9a35bab6f3ae78
Marcelo Tauscher <32394683+masiboss@users.noreply.github.com>
2024-12-04 14:26:55 +0100

    feat: create 04 from template

---
+++ src/bin/day04.rs
@@ -0,0 +1,82 @@
+use adv_code_2024::*;
+use anyhow::*;
+use code_timing_macros::time_snippet;
+use const_format::concatcp;
+use itertools::Itertools;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+const DAY: &str = "04"; // TODO: Fill the day
+#[allow(dead_code)]
+const TEST: Testdata = Testdata {
+    input: "\
+MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX
+",
+    test_result_1: 18,
+    test_result_2: 0,
+};
+const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
+
+#[allow(dead_code)]
+struct Testdata {
+    input: &'static str,
+    test_result_1: usize,
+    test_result_2: usize,
+}
+
+fn main() -> Result<()> {
+    start_day(DAY);
+
+    println!("=== Part 1 ===");
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result1 = time_snippet!(part1(input_file)?);
+    println!("Result = {}", result1);
+
+    println!("\n=== Part 2 ===");
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result2 = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result2);
+
+    Ok(())
+}
+
+fn part1<R: BufRead>(reader: R) -> Result<usize> {
+    let answer = reader.lines().map_while(Result::ok).count();
+    Ok(answer)
+}
+
+fn part2<R: BufRead>(reader: R) -> Result<usize> {
+    let answer = reader.lines().map_while(Result::ok).count();
+    Ok(answer)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(
+            TEST.test_result_1,
+            time_snippet!(part1(BufReader::new(TEST.input.as_bytes())).unwrap())
+        );
+    }
+    #[test]
+    fn test_part2() {
+        assert_eq!(
+            TEST.test_result_2,
+            time_snippet!(part2(BufReader::new(TEST.input.as_bytes())).unwrap())
+        );
+    }
+}
